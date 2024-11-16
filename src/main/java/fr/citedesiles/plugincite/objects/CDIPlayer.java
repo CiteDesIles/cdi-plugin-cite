8d90e940d6275e6b5fc74e786bcfafffc9836914
Azrotho <azrotho.yt@gmail.com>
2024-11-16 09:58:36 +0100

    CDIPlayer & CDITeam

--- src/main/java/fr/citedesiles/plugincite/objects/CDIPlayer.java
+++ src/main/java/fr/citedesiles/plugincite/objects/CDIPlayer.java
@@ -1,4 +1,33 @@
 package fr.citedesiles.plugincite.objects;
 
+import java.util.UUID;
+
 public class CDIPlayer {
+    private UUID uuid;
+    private String team;
+
+    public CDIPlayer(UUID uuid, String team) {
+        this.uuid = uuid;
+        this.team = team;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public boolean hasTeam() {
+        return team != null;
+    }
 }
--- src/main/java/fr/citedesiles/plugincite/objects/CDITeam.java
+++ src/main/java/fr/citedesiles/plugincite/objects/CDITeam.java
@@ -1,4 +1,90 @@
 package fr.citedesiles.plugincite.objects;
 
+import java.util.List;
+import java.util.UUID;
+
 public class CDITeam {
+    private String name;
+    private String displayName;
+    private UUID owner;
+    private List<UUID> members;
+    private long money;
+    private int supportPoints;
+
+    public CDITeam(String name, String displayName, UUID owner, List<UUID> members, long money, int supportPoints) {
+        this.name = name;
+        this.displayName = displayName;
+        this.owner = owner;
+        this.members = members;
+        this.money = money;
+        this.supportPoints = supportPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public UUID getOwner() {
+        return owner;
+    }
+
+    public List<UUID> getMembers() {
+        return members;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public int getSupportPoints() {
+        return supportPoints;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setOwner(UUID owner) {
+        this.owner = owner;
+    }
+
+    public void setMembers(List<UUID> members) {
+        this.members = members;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public void setSupportPoints(int supportPoints) {
+        this.supportPoints = supportPoints;
+    }
+
+    public void addMoney(long money) {
+        this.money += money;
+    }
+
+    public void removeMoney(long money) {
+        this.money -= money;
+    }
+
+    public void addSupportPoints(int supportPoints) {
+        this.supportPoints += supportPoints;
+    }
+
+    public void removeSupportPoints(int supportPoints) {
+        this.supportPoints -= supportPoints;
+    }
+
+    public void addMember(UUID member) {
+        members.add(member);
+    }
+
+    public void removeMember(UUID member) {
+        members.remove(member);
+    }
 }
