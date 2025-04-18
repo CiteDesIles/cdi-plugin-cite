eac7afd1b77a4369a24645978331e6439c7d2987
Raraph84 <contact@raraph.fr>
2025-04-18 22:17:19 +0200

    Fix

--- src/main/java/fr/citedesiles/plugincite/mysql/TeamSyncSQL.java
+++ src/main/java/fr/citedesiles/plugincite/mysql/TeamSyncSQL.java
@@ -31,12 +31,12 @@ public class TeamSyncSQL {
                 Connection connectionPlayer = DatabaseManager.MAIN_DB.getDatabaseAccess().getConnection();
                 PreparedStatement preparedStatementPlayer = connectionPlayer.prepareStatement("SELECT * FROM PLAYER WHERE team = ?");
                 preparedStatementPlayer.setString(1, name);
-                connectionPlayer.close();
                 ResultSet resultSetPlayer = preparedStatementPlayer.executeQuery();
                 while (resultSetPlayer.next()) {
                     UUID uuid = UUID.fromString(resultSetPlayer.getString("uuid"));
                     cdiTeam.getMembers().add(uuid);
                 }
+                connectionPlayer.close();
                 plugin.getLogger().info("Team " + name + " loaded");
             }
             connection.close();
