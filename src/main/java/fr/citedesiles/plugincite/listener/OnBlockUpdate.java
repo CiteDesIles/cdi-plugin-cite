a4f89193e00ce47c96c0893a54c94ca1eecb6593
Raraph84 <contact@raraph.fr>
2025-04-20 22:13:37 +0200

    Disable block updates for team 4

--- src/main/java/fr/citedesiles/plugincite/listener/OnBlockUpdate.java
+++ src/main/java/fr/citedesiles/plugincite/listener/OnBlockUpdate.java
@@ -2,6 +2,7 @@ package fr.citedesiles.plugincite.listener;
 
 import fr.citedesiles.plugincite.PluginCite;
 import fr.citedesiles.plugincite.objects.CDIPlayer;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
@@ -13,22 +14,22 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class OnBlockUpdate implements Listener {
     @EventHandler
     public void onBlockFromTo(BlockFromToEvent event) {
-        if (!PluginCite.instance().islandManager().isInIsland(event.getBlock().getLocation())) event.setCancelled(true);
+        if (areUpdatesDisabled(event.getBlock().getLocation())) event.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockFade(BlockFadeEvent event) {
-        if (!PluginCite.instance().islandManager().isInIsland(event.getBlock().getLocation())) event.setCancelled(true);
+        if (areUpdatesDisabled(event.getBlock().getLocation())) event.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event) {
-        if (!PluginCite.instance().islandManager().isInIsland(event.getBlock().getLocation())) event.setCancelled(true);
+        if (areUpdatesDisabled(event.getBlock().getLocation())) event.setCancelled(true);
     }
 
     @EventHandler
     public void onEntityChangeBlock(EntityChangeBlockEvent event) {
-        if (!PluginCite.instance().islandManager().isInIsland(event.getBlock().getLocation())) event.setCancelled(true);
+        if (areUpdatesDisabled(event.getBlock().getLocation())) event.setCancelled(true);
     }
 
     @EventHandler
@@ -38,5 +39,10 @@ public class OnBlockUpdate implements Listener {
         if (team == null) return;
         CDIPlayer cdiPlayer = PluginCite.instance().playerManager().get(event.getPlayer().getUniqueId());
         if (!team.equals(cdiPlayer.getTeam())) event.setCancelled(true);
+    }
+
+    private boolean areUpdatesDisabled(Location location) {
+        String team = PluginCite.instance().islandManager().getIsland(location);
+        return team == null || team.equals("team4");
     }
 }
